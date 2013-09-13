3c043bc09b0e8610a436815cd45e6c57907c8314
Santosh Moktan <itmyprofession@gmail.com>
2013-09-13 14:58:28 +0545

    User module created

---
+++ application/modules/user/controllers/IndexController.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * IndexController
+ * 
+ * @module User
+ * @category module
+ * @author Santosh Moktan <itmyprofession@gmail.com>
+ * @copyright (c) 2013, 
+ */
+class User_IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    /**
+     * Show list of users
+     */
+    public function indexAction()
+    {
+        // action body
+    }
+
+    /**
+     * Display user registration form
+     */
+    public function registerAction()
+    {
+        // action body
+    }
+
+}
+
