8503b30120eb708142f75ceb822ac5dc515bf7d8
Santosh Moktan <itmyprofession@gmail.com>
2013-09-15 10:46:33 +0545

    save,edit,delete and list action added

--- application/modules/user/controllers/IndexController.php
+++ application/modules/user/controllers/IndexController.php
@@ -2,11 +2,13 @@
 
 /**
  * IndexController
- * 
+ *
  * @module User
  * @category module
  * @author Santosh Moktan <itmyprofession@gmail.com>
  * @copyright (c) 2013, 
+ *
+ *
  */
 class User_IndexController extends Zend_Controller_Action
 {
@@ -18,14 +20,20 @@ class User_IndexController extends Zend_Controller_Action
 
     /**
      * Show list of users
+     *
+     *
      */
     public function indexAction()
     {
-        // action body
+        $user = new User_Service_UserService();
+        $page = $this->_getParam('page', 1);
+        $this->view->users = $user->getUsers($page);
     }
 
     /**
      * Display user registration form
+     *
+     *
      */
     public function registerAction()
     {
@@ -37,10 +45,63 @@ class User_IndexController extends Zend_Controller_Action
                 $userData = new User_Model_User($form->getValues());
                 $mapper = new User_Model_UserMapper();
                 $mapper->save($userData);
+
+                $this->_helper->flashMessenger->addMessage(array('success' => 'User has been successfully saved.'));
                 return $this->_helper->redirector('index');
+            }
+            $this->_helper->flashMessenger->addMessage(array('error' => 'User cannot be saved.'));
+        }
+        $this->view->form = $form;
+    }
+
+    /**
+     * Edit user
+     */
+    public function editAction()
+    {
+        // action body
+        $request = $this->getRequest();
+        $form = new User_Form_Registration();
+        $form->save->setLabel('Save');
+        
+        if ($this->getRequest()->isPost()) {
+            if ($form->isValid($request->getPost())) {
+                $userData = new User_Model_User($form->getValues());
+                $mapper = new User_Model_UserMapper();
+                $mapper->save($userData);
+
+                $this->_helper->flashMessenger->addMessage(array('success' => "User($userData->email) has been successfully updated."));
+                return $this->_helper->redirector('index');
+            }
+            $this->_helper->flashMessenger->addMessage(array('error' => 'User cannot be updated.'));
+        } else {
+            $id = $this->_getParam('id', 0);
+            if ($id > 0) {
+                $user = new User_Model_UserMapper;
+                $form->populate($user->findUser($id));
             }
         }
         $this->view->form = $form;
+    }
+
+    /**
+     * Delete user
+     * 
+     * @throws Exception Incase id is not passed
+     */
+    public function deleteAction()
+    {
+        $id = $this->_getParam('id');
+        if (!$id) {
+            throw new Exception('Invalid argument exception');
+        }
+        $userMapper = new User_Model_UserMapper();
+        if ($userMapper->delete($id)) {
+            $this->_helper->flashMessenger->addMessage(array('success' => "User has been successfully deleted."));
+        } else {
+            $this->_helper->flashMessenger->addMessage(array('warning' => "Something weird happened please try again"));
+        }
+        return $this->_helper->redirector('index');
     }
 
 }
