8b4deb95a15991d7b86a152d0ad5b671c75757af
Santosh Moktan <itmyprofession@gmail.com>
2013-09-15 10:48:39 +0545

    save, edit and delete business logic added here

--- application/modules/user/models/UserMapper.php
+++ application/modules/user/models/UserMapper.php
@@ -47,8 +47,63 @@ class User_Model_UserMapper
             'phone' => $user->getPhone(),
             'country' => $user->getCountry()
         );
+        if (($id = $user->getId()) != 0 && $id != '') {
+            return $this->getDbTable()->update($data, 'id=' . $id);
+        } else {
+            return $this->getDbTable()->insert($data);
+        }
+    }
 
-        return $this->getDbTable()->insert($data);
+    /**
+     * Delete user
+     * 
+     * @param type $id
+     */
+    public function delete($id)
+    {
+        return $this->getDbTable()->delete('id=' . $id);
+    }
+
+    /**
+     * Fetch all the users
+     * 
+     * @return \User_Model_User
+     */
+    public function fetchAll()
+    {
+        $resultSet = $this->getDbTable()->fetchAll();
+        $users = array();
+        foreach ($resultSet as $row) {
+            $user = new User_Model_User();
+            $user->setId($row->id)
+                    ->setEmail($row->email)
+                    ->setFirstname($row->firstname)
+                    ->setLastname($row->lastname)
+                    ->setAddress($row->address)
+                    ->setPhone($row->phone);
+            $users[] = $user;
+        }
+        return $users;
+    }
+
+    /**
+     * Find user by id
+     * 
+     * @param type $id
+     * @return type
+     * @throws Exception
+     * 
+     * @return array User data
+     */
+    public function findUser($id)
+    {
+        $id = (int) $id;
+        $row = $this->getDbTable()->fetchRow('id = ' . $id);
+
+        if (!$row) {
+            throw new Exception("Could not find row $id");
+        }
+        return $row->toArray();
     }
 
 }
